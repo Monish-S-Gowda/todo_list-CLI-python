3e7b53b179876ca0b75f88e59fd5e41851dd58f5
Monish-S-Gowda <monishsgowda11@gmail.com>
2025-08-27 16:33:12 +0530

    Add files via upload

---
+++ todo_list/to_do.py
@@ -0,0 +1,55 @@
+from rich.console import Console
+from rich.align import Align
+
+console = Console()
+from rich import print
+import time
+from to_do_opp import read, menu, add, remove, history, tick, clear
+
+
+def exit(func):
+    n = ["read", "add", "remove", "history", "tick", "clear"]
+    input(f"press enter key to exit {n[func]} window")
+
+
+def main():
+
+    console.print(
+        Align.center(
+            "[bold][underline][blue]TO[/underline] [underline]DO[/underline] [underline]LIST:[/blue][/underline][/bold]"
+        )
+    )
+    time.sleep(1)
+    while True:
+        a = menu()
+        time.sleep(1)
+        console.clear()
+        if a == 0:
+            read()
+            exit(a)
+        elif a == 1:
+            add()
+            exit(a)
+        elif a == 2:
+            remove()
+            exit(a)
+        elif a == 3:
+            history()
+            exit(a)
+        elif a == 4:
+            tick()
+            exit(a)
+        elif a == 5:
+            clear()
+            exit(a)
+        elif a == 6:
+            
+            print("[green][bold]exiting program ...[/bold][/green]")
+            time.sleep(1)
+            break
+
+    console.clear()
+
+
+if __name__ == "__main__":
+    main()
